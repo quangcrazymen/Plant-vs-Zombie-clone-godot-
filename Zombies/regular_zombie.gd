f091bc4d9f17cab04360481ed5456a77ec94c3e5
quang nguyen <quangcrazymen@gmail.com>
2023-11-02 00:01:05 +0700

    +now zombie can stop and eating plant

--- Zombies/regular_zombie.gd
+++ Zombies/regular_zombie.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
 @export var walking_speed: int = 20
+#@onready var zombie_state_machine = $AnimationTree["parameters/playback"]
+@onready var eating = $Eating
+@onready var walking_sprite = $WalkingSprite
+@onready var animation_player = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	eating.visible = false
 	pass # Replace with function body.
 
 
@@ -14,11 +19,16 @@ func _process(delta):
 
 
 func _on_body_area_entered(area):
-	
 	print("Zombie gets hit")
-	pass # Replace with function body.
 
 
 func _on_touch_plant_area_entered(area):
+	#zombie_state_machine.travel("eating")
+	stop_and_eat()
+	animation_player.play("eating")
 	print("Eating plant....")
-	pass # Replace with function body.
+
+func stop_and_eat():
+	walking_speed = 0
+	eating.visible = true
+	walking_sprite.visible = false
