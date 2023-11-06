28b3d49ac547cda7cc03f320f8e6d6f359e2609a
quang nguyen <quangcrazymen@gmail.com>
2023-11-06 23:24:56 +0700

    + Now peashooter can shoot

--- Plants/Peashooter/Peashooter.gd
+++ Plants/Peashooter/Peashooter.gd
@@ -3,28 +3,31 @@ extends Node2D
 @export var bullet_speed: float = 500.0
 @export var bullet_direction: Vector2 = Vector2.RIGHT
 @export var bullet_wait_time: float = 3.0
+@export var bullet_damage: int = 10 
 @export var default_health: int = 100
-@onready var bullet = get_child(2)
+#@onready var bullet = get_child(2)
 # Called when the node enters the scene tree for the first time.
+@export var bullet: PackedScene
 
 var health:int = default_health
 var beingEaten:bool = false
 
 func _ready():
-	print(get_child(2).name)
+	#print(get_child(2).name)
 	pass # Replace with function body.
 
 #func start_sho
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if bullet!=null and bullet.is_queued_for_deletion() == false:
-		bullet.position += bullet_direction*bullet_speed*delta
+#	if bullet!=null and bullet.is_queued_for_deletion() == false:
+#		bullet.position += bullet_direction*bullet_speed*delta
+	#shoot()
 	if beingEaten:
 		dying(delta)
 	if health < 0:
 		die()
-	print("Plant health is %f", health)
+	#print("Plant health is %f", health)
 
 
 func _on_plant_body_area_entered(area):
@@ -32,8 +35,23 @@ func _on_plant_body_area_entered(area):
 	beingEaten = true
 	pass # Replace with function body.
 	
+func shoot():
+	var b = bullet.instantiate()
+	b.setup(
+		global_position,
+		bullet_direction,
+		bullet_speed,
+		bullet_damage
+	)
+	get_tree().current_scene.add_child(b)
+	
 func dying(delta):
 	health-=delta*100
 	
 func die():
 	queue_free()
+
+func _on_peashoter_timer_timeout():
+	print("shoot")
+	shoot()
+	pass # Replace with function body.
--- Plants/Peashooter/bullet.gd
+++ Plants/Peashooter/bullet.gd
@@ -1,14 +1,22 @@
 extends Sprite2D
 
+var _direction: Vector2 = Vector2.UP
+var _speed: float = 200.0
+var _damage: int = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func setup(pos: Vector2, dir: Vector2, sp: float, dmg: int) -> void:
+	_direction = dir
+	_speed = sp
+	_damage = dmg
+	global_position = pos
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	position += _direction * _speed * delta
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	print("Bullet has gone out off screen")
