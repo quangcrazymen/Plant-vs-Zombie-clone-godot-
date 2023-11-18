8313e7e7f527b4304a6566b01593a7506938aa83
quang nguyen <quangcrazymen@gmail.com>
2023-11-18 12:11:11 +0700

    + now Sunflower can drop sun + Plants are disabled when gathered sun is not sufficient

--- Plants/Sunflower/Sunflower.gd
+++ Plants/Sunflower/Sunflower.gd
@@ -1,11 +1,35 @@
 extends Node2D
 
+@export var sun_scene:PackedScene = preload("res://GenericObjects/Sun.tscn")
+@onready var sunflower_timer = $SunflowerTimer
 
+var health:int = 100
+var beingEaten:bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Utils.set_and_start_timer(sunflower_timer,5.,0)
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if beingEaten:
+		dying(delta)
+	if health < 0:
+		die()	
+		
+func dying(delta):
+	health-=delta*100
+
+func die():
+	queue_free()
+
+
+func _on_plant_body_area_entered(area):
+	beingEaten = true
+
+func _on_sunflower_timer_timeout():
+	produce_sun()
+	
+func produce_sun():
+	var new_sun = sun_scene.instantiate()
+	get_tree().current_scene.add_child(new_sun)
+	new_sun.global_position = global_position
--- UI/PlantSelectionTray.gd
+++ UI/PlantSelectionTray.gd
@@ -1,13 +1,18 @@
 extends Control
 
-@onready var plantsSelection = $HBoxContainer
+@onready var plant_selection = $HBoxContainer
 @onready var sun_manager = $"../PcComputer-PlantsVsZombies-Day/SunManager"
 @export var plant_card: PackedScene = preload("res://UI/PlantCard.tscn")
-# Called when the node enters the scene tree for the first time.
 
-var plant_in_game = ["Peashooter","Sunflower"]
+
+#var plants_in_game = ["Peashooter","Sunflower"]
+var plants_in_game = {
+	"Peashooter": 100,
+	"Sunflower": 50
+}
+
 func _ready():
-	for plant in plant_in_game:
+	for plant in plants_in_game:
 		var new_card = plant_card.instantiate()
 		var name_of_resource_card_file = "res://Plants/%s/%sCard.png" % [plant,plant]
 		var name_of_resource_disabled_card_file = "res://Plants/%s/%sCardDisable.png" % [plant,plant]		
@@ -15,20 +20,25 @@ func _ready():
 		new_card.texture_pressed = load(name_of_resource_card_file)		
 		new_card.texture_disabled = load(name_of_resource_disabled_card_file)		
 		new_card.plant_name = plant
-		plantsSelection.add_child(new_card)
-	
-func _process(delta):
-#	if sun_manager.sun_gathered <100:
-#		pea_shooter_card.disabled = true
-#	else:
-#		pea_shooter_card.disabled = false
-	pass
-# Create draggable object: https://www.youtube.com/watch?v=uhgswVkYp0o
+		plant_selection.add_child(new_card)
 
+func _physics_process(_delta):
+	print(sun_manager.sun_gathered)
+	update_card_state()
 
 func _on_texture_rect_mouse_entered():
 	print('mouse enter the card')
 
-
 func _on_texture_button_button_down():
 	print('hold down the card')
+	
+func update_card_state():
+	var plant_cards = plant_selection.get_children()
+	for i in len(plant_cards):
+		if plants_in_game[plant_cards[i].plant_name] > sun_manager.sun_gathered:
+			plant_cards[i].disabled = true
+		else:
+			plant_cards[i].disabled = false
+
+		
+
