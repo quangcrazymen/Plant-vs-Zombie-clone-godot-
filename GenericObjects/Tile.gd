4563f80be023bcce7fb9110363858e77a8222bd9
quang nguyen <quangcrazymen@gmail.com>
2023-11-21 08:09:54 +0700

    - Cleanup some code

--- GenericObjects/Tile.gd
+++ GenericObjects/Tile.gd
@@ -4,12 +4,10 @@ class_name Tile
 
 var on_chosen = false
 var is_occupied = false
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
